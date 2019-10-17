bb5a4f5a6e66862ebc6f7ca5c90d95e0f7add315
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-10-17 11:52:31 -0500

    Fixed container width on Item component

--- src/Item.js
+++ src/Item.js
@@ -52,55 +52,57 @@ class Item extends React.Component {
 
         return (
             <React.Fragment>
-                <header className="row app-header">
-                    <div className="app-title col-md-3">
-                        <h1>OER Search Engine</h1>
-                    </div>
-                    <div className="col-md-9">
-                        <form action="/" method="get" onSubmit={this.handleSubmit.bind(this)}>
-                            <input type="text"
-                                   className="search-bar"
-                                   value={this.state.title}
-                                   onChange={this.handleChange.bind(this)}
-                                   placeholder="Search for OERs"
-                                   name="SearchQuery" id="query"/>
-                        </form>
-                    </div>
-                </header>
-                <div>
-                    {/*Display a message if we encounter an error*/}
-                    {error ? <p>{error.message}</p> : null}
-
-                    {/*// Here's our data check*/}
-                    {!isLoading ? (
-                        <div className="container">
-                            <div className="row">
-                                <h1 className="oer-title">{oer.title}</h1>
-                            </div>
-
-                            <div className="row">
-                                <ul className="oer-details">
-                                    <li><b>Material: </b>{oer.materialType}</li>
-                                    <li><b>Published on: </b>{oer.date}</li>
-                                    <li><b>Views: </b>{oer.views}</li>
-                                </ul>
-                            </div>
-                            <div className="row oer-content">
-                                <img src={oer.imageLink}
-                                     className="col-md-3 oer-image"
-                                     alt={oer.title}/>
-                                     <div className="col-md-9">
-                                         <p className="oer-description">
-                                             {oer.about ? oer.about : "No description"}
-                                         </p>
-                                         <a href={oer.uri} className="btn btn-dark">Source</a>
-                                     </div>
-                            </div>
+                <div className="container-fluid">
+                    <header className="row app-header">
+                        <div className="app-title col-md-3">
+                            <h1>OER Search Engine</h1>
                         </div>
-                        // If there is a delay in data, let's let the user know it's loading
-                    ) : (
-                        <h3>Loading...</h3>
-                    )}
+                        <div className="col-md-9">
+                            <form action="/" method="get" onSubmit={this.handleSubmit.bind(this)}>
+                                <input type="text"
+                                       className="search-bar"
+                                       value={this.state.title}
+                                       onChange={this.handleChange.bind(this)}
+                                       placeholder="Search for OERs"
+                                       name="SearchQuery" id="query"/>
+                            </form>
+                        </div>
+                    </header>
+                    <div>
+                        {/*Display a message if we encounter an error*/}
+                        {error ? <p>{error.message}</p> : null}
+
+                        {/*// Here's our data check*/}
+                        {!isLoading ? (
+                            <div className="container">
+                                <div className="row">
+                                    <h1 className="oer-title">{oer.title}</h1>
+                                </div>
+
+                                <div className="row">
+                                    <ul className="oer-details">
+                                        <li><b>Material: </b>{oer.materialType}</li>
+                                        <li><b>Published on: </b>{oer.date}</li>
+                                        <li><b>Views: </b>{oer.views}</li>
+                                    </ul>
+                                </div>
+                                <div className="row oer-content">
+                                    <img src={oer.imageLink}
+                                         className="col-md-3 oer-image"
+                                         alt={oer.title}/>
+                                    <div className="col-md-9">
+                                        <p className="oer-description">
+                                            {oer.about ? oer.about : "No description"}
+                                        </p>
+                                        <a href={oer.uri} className="btn btn-dark">Source</a>
+                                    </div>
+                                </div>
+                            </div>
+                            // If there is a delay in data, let's let the user know it's loading
+                        ) : (
+                            <h3>Loading...</h3>
+                        )}
+                    </div>
                 </div>
             </React.Fragment>
 
